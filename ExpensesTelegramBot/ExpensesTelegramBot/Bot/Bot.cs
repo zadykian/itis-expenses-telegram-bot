b5fb264b0031a9dbe5294cb8afe41bca582f1437
Alexander Zadykian <zadykian@outlook.com>
2018-12-05 20:15:08 +0300

    Initial commit

---
+++ ExpensesTelegramBot/ExpensesTelegramBot/Bot/Bot.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ExpensesBotApi
+{
+    public class Bot : IBot
+    {
+        public async Task OnTurnAsync(ITurnContext turnContext, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            if (turnContext.Activity.Type is ActivityTypes.Message)
+            {
+                var input = turnContext.Activity.Text;
+                await turnContext.SendActivityAsync($"You said \"{input}\".");
+            }
+        }
+    }
+}
---
+++ ExpensesTelegramBot/ExpensesTelegramBot/Startup.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace ExpensesBotApi
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment environment)
+        {
+            ContentRootPath = environment.ContentRootPath;
+        }
+
+        public string ContentRootPath { get; private set; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(ContentRootPath)
+                .AddJsonFile("appsettings.json")
+                .AddEnvironmentVariables();
+            var configuration = builder.Build();
+            services.AddSingleton(configuration);
+
+            services.AddBot<Bot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(configuration);
+            });
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseHsts();
+            }
+
+            app.UseBotFramework();
+        }
+    }
+}
